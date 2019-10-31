c31b29432a2423b3835273d6150a2c81e74e945d
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-10-31 11:42:40 -0300

    create register form

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <h2>Criar Conta</h2>
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e -mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
