9bf87b7d67fcb4b53638b499cc92752738c17a60
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-10-31 12:16:55 -0300

    add unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.log(data);
+    console.tron.log(data);
+  }
   return (
     <>
       <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.log(data);
+    console.tron.log(data);
+  }
   return (
     <>
       <h2>Criar Conta</h2>
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e -mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e -mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
