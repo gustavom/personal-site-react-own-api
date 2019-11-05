8e0552d8937351776c8cc98b1a5195fc29912834
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-05 17:43:59 -0300

    header core init

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <h1>Gustavo</h1>
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Gustavo Martusewicz</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Gustavo Martusewicz"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
