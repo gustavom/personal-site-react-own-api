0fed00af988c31bd3876e7bdd6e9a029ed6f356d
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-06 10:59:25 -0300

    conditional css if component has props hasUnread

---
+++ src/components/Notifications/styles.js
@@ -0,0 +1,14 @@
+  ${props =>
+    props.hasUnread &&
+    css`
+      &::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 8px;
+        height: 8px;
+        background: #ff892e;
+        border-radius: 50%;
+      }
+    `}
