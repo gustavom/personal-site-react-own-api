dccb4051f0c44566ec746fbd57af3bbfeb55d5c3
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-14 17:01:43 -0300

    profile header update infos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
 
 import Notifications from '~/components/Notifications';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -16,12 +18,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Gustavo Martusewicz</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Gustavo Martusewicz"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
