d144bb8e1f1fa79d140b102eb6a217a7776e9385
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-18 14:32:01 -0300

    add load projects

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    async function loadProjects() {
+      const response = await api.get('projects');
+      const data = response.data.map(project => {
+        return project;
+      });
+      setProjects(data);
+    }
+    loadProjects();
+  }, []);
+
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+      {projects.map(project => (
+        <div key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      ))}
+    </Container>
+  );
 }
