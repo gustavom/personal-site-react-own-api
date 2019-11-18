f5f820a70c74771eb965eff9aab31794d55666f6
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-18 14:41:16 -0300

    add login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/login" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
