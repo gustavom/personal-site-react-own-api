7ec5a65c0a22ed941e5a1ec2613465a9b62ee28f
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-18 16:36:37 -0300

    banner input componet

---
+++ src/components/BannerInput/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useField } from '@rocketseat/unform';
+import api from '~/services/api';
+import { toast } from 'react-toastify';
+
+import { MdAddAPhoto } from 'react-icons/md';
+
+import { Container } from './styles';
+
+export default function BannerInput() {
+  const { defaultValue, registerField } = useField('banner');
+
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      registerField({
+        name: 'banner_id',
+        ref: ref.current,
+        path: 'dataset.file',
+      });
+    }
+  }, [ref.current]); //eslint-disable-line
+
+  async function handleChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]);
+
+    try {
+      const response = await api.post('files', data);
+
+      const { id, url } = response.data;
+
+      setFile(id);
+      setPreview(url);
+    } catch (err) {
+      toast.error(`Whoops! Internal server error.${err}`);
+    }
+  }
+
+  return (
+    <Container>
+      <label htmlFor="banner">
+        {preview && <img src={preview} alt="" />}
+
+        {!preview && (
+          <div className="icon-add">
+            <MdAddAPhoto size={48} color="rgba(0, 0, 0, .7)" />
+          </div>
+        )}
+
+        <input
+          type="file"
+          id="banner"
+          accept="image/*"
+          data-file={file}
+          onChange={handleChange}
+          ref={ref}
+        />
+      </label>
+    </Container>
+  );
+}
