3826023767b286b1454ad53368cbd42129d90d17
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-18 15:17:24 -0300

    home layout styles

---
+++ src/pages/_layouts/home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+// import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  /* background: linear-gradient(-90deg, #7159c1, #ab59c1); */
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Content = styled.div``;
