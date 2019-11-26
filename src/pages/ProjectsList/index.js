27424fbb8569eeb52e0ea1d0a8be38e13c8bd9dc
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-11-26 16:49:22 -0300

    projects dash

--- src/pages/ProjectsList/index.js
+++ src/pages/ProjectsList/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '~/services/api';
 import { Container } from './styles';
+import { MdEdit } from 'react-icons/md';
 
 export default function ProjectsList() {
   const [projects, setProjects] = useState([]);
@@ -18,11 +19,29 @@ export default function ProjectsList() {
   return (
     <Container>
       <h1>Projetos</h1>
-      {projects.map(project => (
-        <div key={project.id}>
-          <Link to={`/project/${project.id}/edit`}>{project.name}</Link>
-        </div>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th className="edit">editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map(project => (
+            <tr key={project.id}>
+              <td>
+                <Link to={`/project/${project.id}/edit`}>{project.name}</Link>
+              </td>
+              <td className="edit">
+                <Link to={`/project/${project.id}/edit`}>
+                  <MdEdit size={20} color="#333333" />
+                  Editar
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Container>
   );
 }
--- src/pages/ProjectsList/styles.js
+++ src/pages/ProjectsList/styles.js
@@ -5,4 +5,46 @@ export const Container = styled.div`
   max-width: 1140px;
   margin: 0 auto;
   padding: 35px 15px;
+  table {
+    width: 100%;
+    padding: 0;
+    margin: 50px 0;
+    border-spacing: 0;
+    tr {
+      border-collapse: collapse;
+    }
+    th,
+    td {
+      border: solid 1px #999;
+      border-collapse: collapse;
+      padding: 8px 16px;
+      text-transform: uppercase;
+      font-weight: 600;
+      &.edit {
+        min-width: 20px;
+        max-width: 20px;
+
+        font-size: 13px;
+        a {
+          display: flex;
+          align-items: center;
+          svg {
+            margin-right: 10px;
+          }
+        }
+      }
+      a {
+        color: #333;
+      }
+    }
+    th {
+      text-align: left;
+      color: #fff;
+      background-color: #222;
+      border-color: #222;
+      &.edit {
+        text-align: center;
+      }
+    }
+  }
 `;
