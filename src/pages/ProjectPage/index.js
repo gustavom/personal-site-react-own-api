dde1de24612b61b76f1fb3bd69a266e148a71e8b
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-03 23:17:57 -0300

    create project page

---
+++ src/pages/ProjectPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function ProjectPage() {
+  return (
+    <Container>
+      <h1>Project Page</h1>
+    </Container>
+  );
+}
