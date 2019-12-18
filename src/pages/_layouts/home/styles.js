01b5a4d2bb648fa03c7b0bfe6e66335e9427c631
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-17 23:46:46 -0300

    remove height 100%

--- src/pages/_layouts/home/styles.js
+++ src/pages/_layouts/home/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 // import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  /* height: 100%; */
   /* background: linear-gradient(-90deg, #7159c1, #ab59c1); */
   background: #fff;
   display: flex;
