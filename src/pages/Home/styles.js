9a40b9bfc4bb46b33a95c6c9c7e8373d62f04fef
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-19 17:49:31 -0300

    add wrapper element and some styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 export const Container = styled.div`
   width: 100%;
+  height: 100vh;
   /* height: 100%; */
   padding: 15px;
   margin: 0 auto;
@@ -9,6 +15,22 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
+
+  &.home-about {
+    background-color: #222;
+    color: #fff;
+    h1 {
+      text-transform: uppercase;
+    }
+    a {
+      color: #fff;
+      padding: 8px 16px;
+      border: solid 1px #fff;
+      border-radius: 90px;
+      text-transform: uppercase;
+      letter-spacing: 0.5px;
+    }
+  }
 `;
 
 export const ProjectItem = styled.div`
