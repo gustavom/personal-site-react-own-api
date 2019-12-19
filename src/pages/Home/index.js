5a474652b49fc56631c3b33d0dc6c7878bc75c79
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-19 17:49:51 -0300

    add about container section and fragment to wrap all

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '~/services/api';
 
-import { Container, ProjectItem } from './styles';
+import { Wrapper, Container, ProjectItem } from './styles';
 
 export default function Home() {
   const [projects, setProjects] = useState([]);
+
   useEffect(() => {
     async function loadProjects() {
       const response = await api.get('projects');
@@ -17,15 +18,27 @@ export default function Home() {
     loadProjects();
   }, []);
   return (
-    <Container>
-      {projects.map(project => (
-        <ProjectItem key={project.id}>
-          <Link to={project.slug}>
-            <h3>{project.name}</h3>
-            {/* <img src={project.banner.url} /> */}
-          </Link>
-        </ProjectItem>
-      ))}
-    </Container>
+    <Wrapper>
+      <Container className="home-about">
+        <h1>Front-end developer</h1>
+        <a
+          href="https://www.linkedin.com/in/gustavo-martusewicz/"
+          target="_blank"
+        >
+          linkedin
+        </a>
+      </Container>
+
+      <Container>
+        {projects.map(project => (
+          <ProjectItem key={project.id}>
+            <Link to={project.slug}>
+              <h3>{project.name}</h3>
+              {/* <img src={project.banner.url} /> */}
+            </Link>
+          </ProjectItem>
+        ))}
+      </Container>
+    </Wrapper>
   );
 }
