9dd10e5a2dfc7e5709ba220f5f71301d6f5d8996
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-19 17:50:07 -0300

    add header some styles

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,40 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: fixed;
   width: 100%;
+  height: 80px;
   padding: 15px;
-  flex-grow: 0;
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  z-index: 10;
+  h1 {
+    font-weight: 300;
+    color: rgba(0, 0, 0, 0.5);
+    a {
+      color: inherit;
+    }
+  }
+  nav {
+    ul {
+      li {
+        text-transform: uppercase;
+        font-size: 13px;
+        letter-spacing: 2.5px;
+      }
+    }
+  }
+  &.white {
+    color: #fff;
+    h1 {
+      color: rgba(255, 255, 255, 0.8);
+      a {
+        color: inherit;
+      }
+    }
+    a {
+      color: inherit;
+    }
+  }
 `;
