fd9612838dc5b216fc613ae08d971cfb8e432276
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-12-19 17:49:15 -0300

    add state scroll and function to get scroll

--- src/pages/_layouts/home/index.js
+++ src/pages/_layouts/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Wrapper, Content } from './styles';
@@ -6,9 +6,29 @@ import { Wrapper, Content } from './styles';
 import Header from '~/components/Header';
 
 export default function HomeLayout({ children }) {
+  const [scroll, setScroll] = useState(0);
+  const [headerLayout, setHeaderLayout] = useState('white');
+
+  function handleScroll() {
+    if (
+      document.documentElement.scrollTop >=
+      document.documentElement.clientHeight - 20
+    ) {
+      setScroll(document.documentElement.clientHeight);
+      setHeaderLayout('black');
+    } else {
+      setScroll(document.documentElement.clientHeight);
+      setHeaderLayout('white');
+    }
+  }
+
+  useEffect(() => {
+    window.onscroll = () => handleScroll();
+  }, []);
+
   return (
     <Wrapper>
-      <Header />
+      <Header layoutchange={headerLayout} />
       <Content>{children}</Content>
     </Wrapper>
   );
