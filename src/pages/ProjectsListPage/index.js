8c2df4ce8f19eac0166e1b4acd10e8330850d8e7
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-05 17:24:48 -0300

    projects list page

--- src/pages/ProjectsListPage/index.js
+++ src/pages/ProjectsListPage/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { ContainerProjetos, ProjectList, ProjectItem } from './styles';
 
 export default function ProjectsListPage() {
-  return <h1>Lista de projetos</h1>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function loadProjects() {
+      const response = await api.get('projects');
+      const data = response.data.map(project => {
+        return project;
+      });
+      setProjects(data);
+    }
+    loadProjects();
+  }, []);
+  return (
+    <ContainerProjetos>
+      <h2>Projetos</h2>
+      <ProjectList>
+        {projects.map(project => (
+          <ProjectItem key={project.id}>
+            <Link to={`project/${project.slug}`}>
+              <h3>{project.name}</h3>
+              {/* <img src={project.banner.url} /> */}
+            </Link>
+          </ProjectItem>
+        ))}
+      </ProjectList>
+    </ContainerProjetos>
+  );
 }
---
+++ src/pages/ProjectsListPage/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const ContainerProjetos = styled.div`
+  width: 100%;
+  height: 100vh;
+  /* height: 100%; */
+  padding: 40px 15px 30px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  h2 {
+    font-family: 'Syncopate', sans-serif;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    border-bottom: solid 3px #222;
+    width: 100%;
+    font-size: 45px;
+    padding-bottom: 15px;
+    margin-bottom: 45px;
+  }
+`;
+export const ProjectList = styled.ul`
+  list-style: none;
+  width: 100%;
+  display: flex;
+  /* flex-direction: column; */
+  align-items: flex-end;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding-left: 50%;
+`;
+export const ProjectItem = styled.li`
+  flex: 0 0 100%;
+  max-width: 100%;
+  text-transform: uppercase;
+  /* padding-bottom: 25px; */
+  border-bottom: solid 1px #999;
+  padding: 10px 3px 6px;
+  margin-bottom: 10px;
+  h3{
+    font-weight: 500;
+    font-size: 14px;
+  }
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
