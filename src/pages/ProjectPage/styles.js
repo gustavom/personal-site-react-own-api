2cb7722a033e889be5020a39f362b9c2f460b9c6
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-06 13:01:58 -0300

    change alignament

--- src/pages/ProjectPage/styles.js
+++ src/pages/ProjectPage/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   /* max-width: 1100px; */
+  height: 100vh;
   margin: 0 auto;
   padding: 50px 15px 10px;
   h1 {
@@ -13,10 +14,14 @@ export const Container = styled.div`
     padding: 0 5px 10px;
     margin-bottom: 10px;
     border-bottom: solid 2px #222;
+    word-break: break-all;
   }
   p {
-    margin: 0 0 25px;
+    margin: 0;
     padding: 0 5px;
+    & + p {
+      margin-top: 15px;
+    }
   }
 `;
 
@@ -25,7 +30,11 @@ export const ProjectDetailContainer = styled.div`
 `;
 export const InfoContainer = styled.div`
   flex: 0 0 50%;
-  padding: 15px;
+  padding: 15px 100px 15px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  /* background-color: red; */
 `;
 export const ImageContainer = styled.div`
   flex: 0 0 50%;
