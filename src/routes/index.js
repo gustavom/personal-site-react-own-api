e3e72e42c46ebc1902aeff05dce698c61185b680
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-13 17:20:53 -0300

    remove unused code

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import Route from './Route';
 
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+// import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
