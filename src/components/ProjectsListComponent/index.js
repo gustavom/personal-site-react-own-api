08e0112830401abcda611d8c53b4b8d8a739ba44
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-18 16:58:37 -0300

    add projects list component loader

--- src/components/ProjectsListComponent/index.js
+++ src/components/ProjectsListComponent/index.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '~/services/api';
+import { toast } from 'react-toastify';
 
 import { Container, ProjectItem } from './styles';
 
+import Loader from '~/components/Loader';
+
 export default function ProjectsListComponent() {
+  const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     async function loadProjects() {
-      const response = await api.get('projects');
-      const data = response.data.map(project => {
-        return project;
-      });
-      setProjects(data);
+      try {
+        const response = await api.get('projects');
+        const data = response.data.map(project => {
+          return project;
+        });
+        setProjects(data);
+        setLoading(false);
+        // setLoading(true);
+      } catch (err) {
+        toast.error('Projects not found');
+      }
     }
     loadProjects();
   }, []);
 
   return (
     <Container>
-      {projects.map(project => (
-        <ProjectItem key={project.id}>
-          <Link to={`project/${project.slug}`}>
-            <h3>{project.name}</h3>
-            {/* <img src={project.banner.url} /> */}
-          </Link>
-        </ProjectItem>
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        projects.map(project => (
+          <ProjectItem key={project.id}>
+            <Link to={`project/${project.slug}`}>
+              <h3>{project.name}</h3>
+              {/* <img src={project.banner.url} /> */}
+            </Link>
+          </ProjectItem>
+        ))
+      )}
     </Container>
   );
 }
