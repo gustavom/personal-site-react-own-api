ad4543d94f074d8e744e4f9c55696859376429b1
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-19 15:22:55 -0300

    project page mobile

--- src/pages/ProjectPage/styles.js
+++ src/pages/ProjectPage/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+const size = {
+  table: '920px',
+};
+
 export const Container = styled.div`
   width: 100%;
   /* max-width: 1100px; */
@@ -24,10 +28,16 @@ export const Container = styled.div`
       margin-top: 15px;
     }
   }
+  @media (max-width: ${size.table}) {
+    padding: 50px 0 10px;
+  }
 `;
 
 export const ProjectDetailContainer = styled.div`
   display: flex;
+  @media (max-width: ${size.table}) {
+    flex-direction: column;
+  }
 `;
 export const InfoContainer = styled.div`
   flex: 0 0 50%;
@@ -37,6 +47,11 @@ export const InfoContainer = styled.div`
   justify-content: flex-end;
   /* background-color: red; */
   animation: fade-in-left 0.8s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  @media (max-width: ${size.table}) {
+    flex: 0 0 100%;
+    order: 2;
+    padding: 50px 30px 0;
+  }
 `;
 export const ImageContainer = styled.div`
   flex: 0 0 50%;
@@ -50,5 +65,9 @@ export const ImageContainer = styled.div`
     display: block;
     width: 100%;
     height: 100%;
+  }
+  @media (max-width: ${size.table}) {
+    flex: 0 0 100%;
+    order: 1;
   }
 `;
