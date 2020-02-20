2fb97b91cc0467d83a6e3d1593dbc90bc1298980
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-19 22:38:09 -0300

    add footer to sample page

--- src/pages/_layouts/sample/index.js
+++ src/pages/_layouts/sample/index.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import { Wrapper, Content } from './styles';
 
 import Header from '~/components/Header';
+import Footer from '~/components/Footer';
 
 export default function SampleLayout({ children }) {
   return (
     <Wrapper>
       <Header />
       <Content>{children}</Content>
+      <Footer />
     </Wrapper>
   );
 }
