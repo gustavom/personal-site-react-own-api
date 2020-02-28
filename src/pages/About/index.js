bf382fadf9e8c2e265d99b6c0f9fb0e5bffb6e0b
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-27 23:44:55 -0300

    add api in about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+import api from '~/services/api';
+import { toast } from 'react-toastify';
 
 import { Wrapper, Container, AboutContainer } from './styles';
+import Loader from '~/components/Loader';
 
-export default function About() {
+export default function About({ history }) {
+  const { slug } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [about, setAbout] = useState(null);
+
+  useEffect(() => {
+    async function loadAbout() {
+      try {
+        const { data } = await api.get(`institucional/sobre`);
+        console.log('1', data);
+        setAbout({
+          ...data,
+        });
+        console.log('2', about);
+        setLoading(false);
+        // setLoading(true);
+      } catch (err) {
+        toast.error('Project not found');
+        history.push('/');
+      }
+    }
+    loadAbout();
+  }, [slug, history]);
+
   return (
     <Wrapper>
       <Container>
         <h1>Sobre</h1>
-        <AboutContainer>
-          <p>Olá!</p>
+
+        {loading ? (
+          // <div className="loading">carregando...</div>
+          <AboutContainer>
+            <Loader />
+          </AboutContainer>
+        ) : (
+          <AboutContainer>
+            {/* <p>Olá!</p>
           <p>
             Sou Gustavo Martusewicz, desenvolvedor front-end.
             <br />
@@ -27,8 +62,10 @@ export default function About() {
             isso) essa maravilhosa lib que é o React JS.
           </p>
           <p>Sinta-se livre para navegar, olhar e avaliar!</p>
-          <p>Obrigado!</p>
-        </AboutContainer>
+          <p>Obrigado!</p> */}
+            <p>{about.content}</p>
+          </AboutContainer>
+        )}
       </Container>
     </Wrapper>
   );
